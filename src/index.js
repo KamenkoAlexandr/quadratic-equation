b2d202d3e421604ef91df9eae92b83d39bce8119
Alexandr <34417026+KamenkoAlexandr@users.noreply.github.com>
2018-02-21 12:03:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+      var len = equation.length;
+    var a = '', b = '', c = '', D, x1, x2;
+    var i = 0;
+    while (equation[i] != '*') {
+        a += equation[i];
+        i++;
+    }
+    a = + a;
+    i = i + 6;
+    while (equation[i] != '*') {
+        if(equation[i] != ' ')
+            b += equation[i];
+        i++;
+    }
+    b = + b;
+    i = i + 4;
+    while (i != len) {
+        if(equation[i] != ' ')
+            c += equation[i];
+        i++;
+    }
+    c = + c;
+    D = (Math.pow(b,2) - 4*a*c);
+
+    x1 = ((-b + Math.sqrt(D))/(2*a));
+    x2 = ((-b - Math.sqrt(D))/(2*a));
+    var arr = [];
+    arr.push(Math.round(x1));
+    arr.push(Math.round(x2));
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a < b) return -1;
+    }
+    arr.sort(compareNumeric);
+  return arr;
 }
